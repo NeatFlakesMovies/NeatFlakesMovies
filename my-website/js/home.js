dc88287dae0a2824006e27461d421491157f13c0
NeatFlakesMovies <siglos_jr@yahoo.com>
2025-05-07 12:11:42 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '2a5ba58445aa360a302f3df6c678a27a';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
